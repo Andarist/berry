40db153c501f36db708f44c809d7c45607c346f7
Mateusz Burzyński <mateuszburzynski@gmail.com>
2026-06-13 00:10:48 +0200

    Add a publishConfig.registry vs yarn npm info test

--- packages/acceptance-tests/pkg-tests-specs/sources/commands/npm/info.test.ts
+++ packages/acceptance-tests/pkg-tests-specs/sources/commands/npm/info.test.ts
@@ -1,4 +1,4 @@
-export {};
+import {tests, yarn} from 'pkg-tests-core';
 
 describe(`Commands`, () => {
   describe(`npm info`, () => {
@@ -39,6 +39,43 @@ describe(`Commands`, () => {
         expect(stdout).toMatchJSON(expect.objectContaining({
           version: `1.0.0-rc.1`,
         }));
+      }),
+    );
+
+    test(
+      `it should use publishConfig.registry rather than the configured fetch registry for workspace packages`,
+      makeTemporaryEnv({
+        name: `@first/pkg`,
+        version: `1.0.0`,
+        publishConfig: {
+          registry: `https://registry.example.org/registry/publish`,
+        },
+      }, async ({path, run, source}) => {
+        const registryUrl = await tests.startPackageServer();
+
+        await yarn.writeConfiguration(path, {
+          npmScopes: {
+            first: {
+              npmRegistryServer: `${registryUrl}/registry/config`,
+            },
+          },
+          npmRegistries: {
+            [`${registryUrl}/registry/publish`]: {
+              npmAuthToken: `publish-token`,
+            },
+          },
+        });
+
+        const requests = await tests.startRegistryRecording(async () => {
+          await expect(run(`npm`, `info`, `.`, `--json`)).rejects.toThrow(/Package not found/);
+        });
+
+        expect(tests.sortJson(requests)).toEqual([{
+          registry: `publish`,
+          scope: `@first`,
+          localName: `pkg`,
+          type: `packageInfo`,
+        }]);
       }),
     );
   });
